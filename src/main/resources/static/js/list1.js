ba498f94a80ea3b1d09e4c47f761aed4acdd34bd
dreamSmile <1056262718@qq.com>
2018-03-06 14:45:53 +0800

    分页ok

--- src/main/resources/static/js/list1.js
+++ src/main/resources/static/js/list1.js
@@ -3,6 +3,11 @@ $(function () {
         $("#bookForm")[0].reset();
     });
     $("#submitBtn").on("click", function () {
+        $("#pageNumber").val(1);
         $("#bookForm").submit();
     });
 });
+function toPage(pageNumber) {
+    $("#pageNumber").val(pageNumber);
+    $("#bookForm").submit();
+}

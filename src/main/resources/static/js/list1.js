62d44269afae298668466b158cd6cdc2868df368
dreamSmile <1056262718@qq.com>
2018-03-06 15:50:03 +0800

    分页ok

--- src/main/resources/static/js/list1.js
+++ src/main/resources/static/js/list1.js
@@ -1,4 +1,16 @@
+var keyWord;
+var minPrice;
+var maxPrice;
+var minTime;
+var maxTime;
+var categoryId;
 $(function () {
+    keyWord = $("#keyWord").val();
+    minPrice = $("#minPrice").val();
+    maxPrice = $("#maxPrice").val();
+    minTime = $("#minTime").val();
+    maxTime = $("#maxTime").val();
+    categoryId = $("#categoryId").val();
     $("#clearBtn").on("click", function () {
         $("#bookForm")[0].reset();
     });
@@ -9,5 +21,11 @@ $(function () {
 });
 function toPage(pageNumber) {
     $("#pageNumber").val(pageNumber);
+    $("#keyWord").val(keyWord);
+    $("#minPrice").val(minPrice);
+    $("#maxPrice").val(maxPrice);
+    $("#minTime").val(minTime);
+    $("#maxTime").val(maxTime);
+    $("#categoryId").val(categoryId);
     $("#bookForm").submit();
 }

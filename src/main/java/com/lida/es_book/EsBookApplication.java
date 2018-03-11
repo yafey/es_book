5342c7857144d17e080d55f0af6bd4a842727a20
Dulida <dulida>
2018-03-11 22:53:39 +0800

    @EnableJms

--- src/main/java/com/lida/es_book/EsBookApplication.java
+++ src/main/java/com/lida/es_book/EsBookApplication.java
@@ -3,9 +3,11 @@ package com.lida.es_book;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.jms.annotation.EnableJms;
 
 @ServletComponentScan //使WebFilter WebServlet等起作用 Druid
 @SpringBootApplication
+@EnableJms
 public class EsBookApplication {
 
 	public static void main(String[] args) {
